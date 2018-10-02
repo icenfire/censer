10c969bfba09368711a8fc0c13c593cab58b487a
chanyoungs <chanyoungs@gmail.com>
2018-10-03 00:13:03 +0100

    Reorganised file structure, added login textfield components, added theme

---
+++ src/components/LoginPage/EmailTextField.tsx
@@ -0,0 +1,24 @@
+import { InputAdornment, TextField } from "@material-ui/core";
+import { EmailOutlined } from "@material-ui/icons";
+import * as React from "react";
+
+export default () => {
+  return (
+    <TextField
+      id="outlined-email-input"
+      label="Email Address"
+      className="email"
+      type="email"
+      name="email"
+      autoComplete="email"
+      margin="normal"
+      InputProps={{
+        endAdornment: (
+          <InputAdornment position="end">
+            <EmailOutlined />
+          </InputAdornment>
+        )
+      }}
+    />
+  );
+};

142fd8e393ad0e2c7759700b809ed1c0b8ba1c8c
chanyoungs <chanyoungs@gmail.com>
2018-10-21 13:53:34 +0100

    Added Appbar with PersistentDrawer

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,12 @@
 import { CssBaseline } from "@material-ui/core";
 import * as React from "react";
+import LeaderMain from "./LeaderMain";
 import LoginPage from "./LoginPage";
 
 export default () => (
   <div>
     <CssBaseline />
     <LoginPage />
+    <LeaderMain />>
   </div>
 );
---
+++ src/components/LeaderMain.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+import AppBarLeaderMain from "./Level1/AppBars/AppBarLeaderMain";
+export default class LoginPage extends React.PureComponent {
+  public render() {
+    return (
+      <div>
+        <AppBarLeaderMain />
+      </div>
+    );
+  }
+}
